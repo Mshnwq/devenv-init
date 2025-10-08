75f5771a3aa45693722692c131de7a797e2fa491
mshnwq <mshnwq.com@gmail.com>
2025-10-08 18:46:35 +0300

    add lua

--- flake.nix
+++ flake.nix
@@ -34,9 +34,58 @@
     };
 }
 
-# TODO:
-# packages.${system}.dclint = pkgs.stdenv.mkDerivation {
-#   pname = "dclint";
-#   version = "1.0";
-#   src = self;
-# };
+# TODO: scout
+# {
+#   description = "Ephemeral docker-scout runner";
+#
+#   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+#
+#   outputs = { self, nixpkgs }:
+#     let
+#       system = "x86_64-linux"; # or builtins.currentSystem
+#       pkgs = import nixpkgs { inherit system; };
+#       tag = "1.18.3";
+#     in {
+#       apps.${system}.scout = {
+#         type = "app";
+#         program = toString (pkgs.writeShellScript "docker-scout-ephemeral" ''
+#           set -euo pipefail
+#
+#           # Correct plugin dir: must be cli-plugins
+#           PLUGIN_DIR="$PWD/.docker/scout"
+#           CONFIG_FILE="$PWD/.docker/config.json"
+#           mkdir -p "$PLUGIN_DIR"
+#
+#           TAR="$PWD/docker-scout-${tag}.tar.gz"
+#
+#           # Download if not cached already
+#           BIN="$PLUGIN_DIR/docker-scout"
+#           if [ ! -x "$BIN" ]; then
+#             URL="https://github.com/docker/scout-cli/releases/download/v${tag}/docker-scout_${tag}_linux_amd64.tar.gz"
+#             echo $URL
+#             curl -L "$URL" -o "$TAR"
+#             mkdir -p "$(dirname "$BIN")"
+#             tar -xf "$TAR" -C "$(dirname "$BIN")" docker-scout
+#             chmod +x "$BIN"
+#             rm "$TAR"
+#           fi
+#
+#           # Minimal config.json (not strictly needed since it's in cli-plugins/, but harmless)
+#           mkdir -p "$(dirname "$CONFIG_FILE")"
+#           cat > "$CONFIG_FILE" <<EOF
+#           {
+#             "cliPluginsExtraDirs": [
+#               "$(dirname "$BIN")"
+#             ]
+#           }
+#           EOF
+#
+#           # Point Docker to use this config
+#           export DOCKER_CONFIG="$PWD/.docker"
+#
+#           # Forward all args to docker scout
+#           exec ${pkgs.docker}/bin/docker "$@"
+#         '');
+#       };
+#     };
+# }
